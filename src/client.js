1dcc4260055ac32463dabf07bccb3c989ea4408e
iamraghavan <developer.raghavan@gmail.com>
2023-12-27 16:48:17 +0530

    clinetjs file update - cloudflare

--- src/client.js
+++ src/client.js
@@ -4,18 +4,3 @@ sapper.start({
 	target: document.querySelector('#sapper')
 });
 
-
-// src/client.js
-
-if (typeof window !== 'undefined') {
-  // Check if the page has been refreshed before
-  const hasRefreshed = sessionStorage.getItem('hasRefreshed');
-
-  if (!hasRefreshed) {
-    // If not refreshed before, refresh the page
-    location.reload(true);
-
-    // Set a flag in sessionStorage indicating that the page has been refreshed
-    sessionStorage.setItem('hasRefreshed', 'true');
-  }
-}
