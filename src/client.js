cdedeacf6d0ff2df1997f31d294b168f6961de9f
iamraghavan <developer.raghavan@gmail.com>
2023-12-27 16:34:37 +0530

    cloudflare01

--- src/client.js
+++ src/client.js
@@ -8,21 +8,14 @@ sapper.start({
 // src/client.js
 
 if (typeof window !== 'undefined') {
-	// Check if the page has been reloaded before
-	const hasReloaded = sessionStorage.getItem('hasReloaded');
-  
-	if (!hasReloaded) {
-	  // If not reloaded before, wait for 2 seconds and then reload the page
-	  setTimeout(() => {
-		location.reload(true);
-  
-		// Set a flag in sessionStorage indicating that the page has been reloaded
-		sessionStorage.setItem('hasReloaded', 'true');
-	  }, 1000);
-	}
-  
-	// Reset the flag when navigating to a new page
-	window.addEventListener('beforeunload', () => {
-	  sessionStorage.removeItem('hasReloaded');
-	});
+  // Check if the page has been refreshed before
+  const hasRefreshed = sessionStorage.getItem('hasRefreshed');
+
+  if (!hasRefreshed) {
+    // If not refreshed before, refresh the page
+    location.reload(true);
+
+    // Set a flag in sessionStorage indicating that the page has been refreshed
+    sessionStorage.setItem('hasRefreshed', 'true');
   }
+}
--- src/routes/who-we-are.svelte
+++ src/routes/who-we-are.svelte
@@ -9,6 +9,20 @@
 </svelte:head>
 
 <script>
+
+if (typeof window !== 'undefined') {
+  // Check if the page has been refreshed before
+  const hasRefreshed = sessionStorage.getItem('hasRefreshed');
+
+  if (!hasRefreshed) {
+    // If not refreshed before, refresh the page
+    location.reload(true);
+
+    // Set a flag in sessionStorage indicating that the page has been refreshed
+    sessionStorage.setItem('hasRefreshed', 'true');
+  }
+}
+
   import Cta from "../components/CTA.svelte";
 import EnquiryForm from "../components/EnquiryForm.svelte";
 
